627517b37455b68d672cc0e85e185a212b2ce1dd
Drew Lytle <aml3ed@virginia.edu>
2023-06-20 15:20:38 -0400

    Actually mock randomization

--- src/test/generateThumbnail.test.ts
+++ src/test/generateThumbnail.test.ts
@@ -7,17 +7,19 @@ import { toMatchImageSnapshot } from 'jest-image-snapshot'
 expect.extend({ toMatchImageSnapshot })
 
 it('should generate thumbnail', async () => {
+  const daysSpy = vi.spyOn(randomizer, 'daysOrHours')
+  daysSpy.mockImplementation(() => 'days')
+
+  const viewSpy = vi.spyOn(randomizer, 'randomNumber')
+  viewSpy.mockImplementation(() => 400)
+
   const thumbnail = await generateThumbnail(
     'This is a really really long title that should take up TWO lines and be truncated',
     'drewlyton',
     'http://mockserver.com/thumbnail.png',
     'http://mockserver.com/profile.jpeg'
   )
-  const daysSpy = vi.spyOn(randomizer, 'daysOrHours')
-  daysSpy.mockImplementation(() => 'days')
 
-  const viewSpy = vi.spyOn(randomizer, 'randomNumber')
-  viewSpy.mockImplementation(() => 400)
   fs.writeFileSync(path.resolve(__dirname, './test-image.png'), thumbnail)
 
   expect(
