045cb3ab478d16db207e8e05559334ea1ba64a71
Keith Stolte <keithrstolte@gmail.com>
2023-06-24 20:26:12 -0400

    log: adding sentry profiling

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import { Client, Collection, GatewayIntentBits } from 'discord.js'
 import * as Sentry from '@sentry/node'
 import { CaptureConsole } from '@sentry/integrations'
+import { ProfilingIntegration } from '@sentry/profiling-node'
+
 import { Commands } from './commands'
 import { getEnv } from './getEnv'
 import interactionCreate from './listeners/interactionCreate'
 import ready from './listeners/ready'
 
 Sentry.init({
-  integrations: [new CaptureConsole()],
+  integrations: [new CaptureConsole(), new ProfilingIntegration()],
   environment: process.env.NODE_ENV,
   release: 'thumbnailed@' + process.env.npm_package_version,
 })
