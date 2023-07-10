65120a9d129966470ed48e607b040e85e19ccc2a
Drew Lytle <aml3ed@virginia.edu>
2023-07-10 18:51:54 -0400

    Update start script

--- apps/discord/start.sh
+++ apps/discord/start.sh
@@ -6,7 +6,7 @@
 # Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
 
 set -ex
-npm run deploy:prod
+pnpm run deploy:prod
 # node ./scripts/deploy-commands.js
 # node ./scripts/deploy-emoji.js
-npm run start
+pnpm run start
