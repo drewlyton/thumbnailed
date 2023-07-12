6507fc7666855029d908a280bbc90c7212f90447
Drew Lytle <aml3ed@virginia.edu>
2023-07-12 17:24:32 -0400

    Update github link

--- apps/web/app/routes/_index.tsx
+++ apps/web/app/routes/_index.tsx
@@ -80,7 +80,7 @@ function NavBar() {
           asChild
           variant={'ghost'}
         >
-          <Link to={'https://google.com'} target="_blank">
+          <Link to={'https://github.com/drewlyton/thumbnailed'} target="_blank">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="1.5em"
