d52614791c80232ec4180e5ceef68762a7f09104
Drew Lytle <aml3ed@virginia.edu>
2023-07-12 09:47:08 -0400

    Fix prettier formatter for tailwind

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,6 @@
 /** @type {import("prettier").Options} */
 module.exports = {
-  // plugins: [require('prettier-plugin-tailwindcss')],
+  plugins: ['prettier-plugin-tailwindcss'],
   singleQuote: true,
   semi: false,
 }
