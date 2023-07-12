4fc230a4225f68cb093d4f03b1ee83fe5c5581e6
Drew Lytle <aml3ed@virginia.edu>
2023-07-12 07:42:43 -0400

    Update dockerfile and start script

--- apps/web/start.sh
+++ apps/web/start.sh
@@ -13,5 +13,6 @@ echo 10 > /proc/sys/vm/swappiness
 swapon /swapfile
 echo 1 > /proc/sys/vm/overcommit_memory
 
-npx prisma migrate deploy
+pnpx prisma generate
+pnpx prisma migrate deploy
 pnpm run start
