08d45ec86cd329e8bf70538178d032114a4807f8
Drew Lytle <aml3ed@virginia.edu>
2023-07-18 07:36:02 -0400

    Update cache strategy again

--- apps/discord/src/index.ts
+++ apps/discord/src/index.ts
@@ -1,6 +1,6 @@
-import { Client, Collection, GatewayIntentBits, Options } from 'discord.js'
-import * as Sentry from '@sentry/node'
 import { CaptureConsole } from '@sentry/integrations'
+import * as Sentry from '@sentry/node'
+import { Client, Collection, GatewayIntentBits, Options } from 'discord.js'
 
 import { Commands } from './commands'
 import { getEnv } from './getEnv'
@@ -14,46 +14,50 @@ Sentry.init({
   profilesSampleRate: 1.0,
 })
 
-const client = Object.assign(
-  new Client({
-    intents: [GatewayIntentBits.Guilds],
-    makeCache: Options.cacheWithLimits({
-      ...Options.DefaultMakeCacheSettings,
-      ApplicationCommandManager: 0,
-      AutoModerationRuleManager: 0,
-      BaseGuildEmojiManager: 0,
-      GuildBanManager: 0,
-      GuildEmojiManager: 0,
-      GuildForumThreadManager: 0,
-      GuildInviteManager: 0,
-      GuildMemberManager: 0,
-      GuildScheduledEventManager: 0,
-      GuildStickerManager: 0,
-      GuildTextThreadManager: 0,
-      MessageManager: 0,
-      PresenceManager: 0,
-      ReactionManager: 0,
-      ReactionUserManager: 0,
-      StageInstanceManager: 0,
-      ThreadManager: 0,
-      ThreadMemberManager: 0,
-      UserManager: 0,
-      VoiceStateManager: 0,
-    }),
-    sweepers: {
-      ...Options.DefaultSweeperSettings,
-      messages: {
-        interval: 3600, // Every hour...
-        lifetime: 1800, // Remove messages older than 30 minutes.
-      },
-      // users: { //commented out due to some tsc weirdness, the client is not yet available here.
-      //   interval: 3600, // Every hour...
-      //   filter: () => user => user.bot && user.id !== client.user.id, // Remove all bots.
-      // },
-    },
+const botClient: Client = new Client({
+  intents: [GatewayIntentBits.Guilds],
+  makeCache: Options.cacheWithLimits({
+    ...Options.DefaultMakeCacheSettings,
+    ApplicationCommandManager: 0,
+    AutoModerationRuleManager: 0,
+    BaseGuildEmojiManager: 0,
+    GuildBanManager: 0,
+    GuildEmojiManager: 0,
+    GuildForumThreadManager: 0,
+    GuildInviteManager: 0,
+    GuildMemberManager: 0,
+    GuildScheduledEventManager: 0,
+    GuildStickerManager: 0,
+    GuildTextThreadManager: 0,
+    MessageManager: 0,
+    PresenceManager: 0,
+    ReactionManager: 0,
+    ReactionUserManager: 0,
+    StageInstanceManager: 0,
+    ThreadManager: 0,
+    ThreadMemberManager: 0,
+    UserManager: 0,
+    VoiceStateManager: 0,
   }),
-  { commands: new Collection() }
-)
+  sweepers: {
+    ...Options.DefaultSweeperSettings,
+    messages: {
+      interval: 1800, // Every 30 minutes...
+      lifetime: 900, // Remove messages older than 15 minutes.
+    },
+    users: {
+      interval: 1800, // Every 30 minutes ...
+      filter: () => (user) => {
+        if (botClient.user) {
+          return user.id !== botClient.user.id
+        } else {
+          return false
+        }
+      }, // Clear everyone except us
+    },
+  },
+})
+const client = Object.assign(botClient, { commands: new Collection() })
 
 for (const command of Commands) {
   client.commands.set(command.data.name, command)
